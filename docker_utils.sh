eed8cff895cd73c9fbef0b28c137b7a33b7fb907
Isabel Restrepo <isabelrestre@gmail.com>
2018-12-10 15:05:32 -0500

    update utils file

--- docker_utils.sh
+++ docker_utils.sh
@@ -1,20 +1,3 @@
-#!/bin/sh
-echo $1
-
-if [ "$1" == "build" ]; then
-    docker build -f Dockerfile --tag=bcbi/solrplant_api .
-elif [ "$1" == "run" ]; then
-    docker run -d -p 8081:8081 --net="host" --restart always --name solrplant_api bcbi/solrplant_api
-elif [ "$1" == "push" ]; then
-    docker push bcbi/solrplant_api
-elif [ "$1" == "pull" ]; then
-    docker pull bcbi/solrplant_api
-elif [ "$1" == "rm" ]; then
-    docker rm bcbi/solrplant_api
-else
-    echo "First argument must be one of the following strings: build, run, run-dev, push, pull, rm"
-fi
-
 #!/bin/bash
 echo $1
 
