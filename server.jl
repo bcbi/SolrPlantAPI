e2dbc03553b797b576d1ee6b014a7ed5602e1d3b
Isabel Restrepo <isabelrestre@gmail.com>
2018-12-12 09:17:00 -0500

    allow GET as well

--- server.jl
+++ server.jl
@@ -28,7 +28,7 @@ function build_server()
         "Date"              => Dates.format(now(Dates.UTC), Dates.RFC1123Format),
         # "Access-Control-Allow-Origin" => "https://bcbi.brown.edu",
         "Access-Control-Allow-Origin" => "*",
-        "Access-Control-Allow-Methods" => "POST" )
+        "Access-Control-Allow-Methods" => "POST, GET" )
 
     
     # route handlers
